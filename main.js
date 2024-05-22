20e0f0027741c9ba13a5873821aaf516ce0af59d
Demigoddd <armen1298@gmail.com>
2024-05-22 10:25:30 +0400

    fix

--- main.js
+++ main.js
@@ -1,16 +1,20 @@
 $(document).ready(function($) {
-	castParallax();
+  castParallax();
 });
 
 function castParallax() {
-	window.addEventListener("scroll", function() {
-		var top = this.scrollY;
-		var layers = document.querySelectorAll(".parallax");
+  if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+    return;
+  } else {
+    window.addEventListener("scroll", function() {
+      var top = this.scrollY;
+      var layers = document.querySelectorAll(".parallax");
 
-    layers.forEach((layer) => {
-      var speed = layer.getAttribute('data-speed');
-      var yPos = -(top * speed / 100);
-      layer.setAttribute('style', 'transform: translate3d(0px, ' + yPos + 'px, 0px)');
+      layers.forEach((layer) => {
+        var speed = layer.getAttribute('data-speed');
+        var yPos = -(top * speed / 100);
+        layer.setAttribute('style', 'transform: translate3d(0px, ' + yPos + 'px, 0px)');
+      });
     });
-	});
+  }
 }
